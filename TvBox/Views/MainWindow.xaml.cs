f5f2d5921928766282f70c19924f1e267c80427d
Chrys2710 <christophe.aroule@gmail.com>
2018-06-09 23:35:57 +0200

    V1
    -window close
    -fullscreen

--- TvBox/Views/MainWindow.xaml.cs
+++ TvBox/Views/MainWindow.xaml.cs
@@ -1,35 +1,25 @@
 ﻿using System;
+using System.Threading;
 using System.Windows;
 
 namespace TvBox.Views
 {
     public partial class MainWindow
     {
+
         public MainWindow()
         {
             InitializeComponent();
 
-            //System.Diagnostics.Process.Start("CMD.exe", "\"C:/Program Files (x86)/Google/Chrome/Application/chrome.exe\" --remote-debugging-port=9222");
             System.Diagnostics.Process.Start("C:\\Users\\Chris\\Documents\\GitHub\\TvBox\\TvBox\\chrome_debug.bat");
-            /*System.Diagnostics.Process process = new System.Diagnostics.Process();
-            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
-            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-            startInfo.FileName = "cmd.exe";
-            startInfo.Arguments = "\"C:/Program Files (x86)/Google/Chrome/Application/chrome.exe\" --remote-debugging-port=9222";
-            process.StartInfo = startInfo;
-            process.Start();*/
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //System.Diagnostics.Process.Start("chrome", "https://www.mycanal.fr/live/&params[tab]=/live-tv/pid5170-live-tv-v2-liste-des-chaines.html&params[filter]=0&params[filters-0$g$]=0$g$2$&params[filters-1$pt$]=current&get=500?epgId=177");
-
+            
             //Chrome tests
             var chrome = new Chrome("http://localhost:9222");
             var sessions = chrome.GetAvailableSessions();
-            //Console.WriteLine("Available debugging sessions");
-            //foreach (var s in sessions)
-            //Console.WriteLine(s.url);
 
             if (sessions.Count == 0)
                 throw new Exception("All debugging sessions are taken.");
@@ -38,13 +28,29 @@ namespace TvBox.Views
             var sessionWSEndpoint = sessions[0].webSocketDebuggerUrl;
             chrome.SetActiveSession(sessionWSEndpoint);
             chrome.NavigateTo("https://www.mycanal.fr/live/&params[tab]=/live-tv/pid5170-live-tv-v2-liste-des-chaines.html&params[filter]=0&params[filters-0$g$]=0$g$2$&params[filters-1$pt$]=current&get=500?epgId=177");
-
-
-
+            
+            //chrome.Eval("document.querySelector('[data-sp-action=\"fullscreenBtPressed\"]').click();");
+            
             //var result = chrome.Eval("document.getElementById('lst-ib').value='Hello World'");
             //result = chrome.Eval("document.forms[0].submit()");
 
             //Console.ReadLine();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            
+            var chrome = new Chrome("http://localhost:9222");
+            var sessions = chrome.GetAvailableSessions();
+
+            if (sessions.Count == 0)
+                throw new Exception("All debugging sessions are taken.");
+
+            // Will drive first tab session
+            var sessionWSEndpoint = sessions[0].webSocketDebuggerUrl;
+            chrome.SetActiveSession(sessionWSEndpoint);
+            chrome.Eval("window.close()");
+            System.Windows.Application.Current.Shutdown();
         }
     }
 }
