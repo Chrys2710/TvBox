a561b2a1027b52d02b24a1dd18e3da0dcfa3d256
Chrys2710 <christophe.aroule@gmail.com>
2018-07-06 22:22:35 +0200

    V1.1
    Controlled by Selenium
    Begin remote command with keyboard remote

---
+++ TvBox/Models/MyCanal.cs
@@ -0,0 +1,66 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TvBox.Models
+{
+    class MyCanal
+    {
+        public Dictionary<int, string> chaines = new Dictionary<int, string>()
+        {
+            { 4, "CHN43FN_301_20170719"}
+        };
+
+        public void testCanalPlus(int chaine)
+        {
+            //string url = "https://www.mycanal.fr/live/&params[tab]=/live-tv/pid5170-live-tv-v2-liste-des-chaines.html&params[filter]=0&params[filters-0$g$]=0$g$2$&params[filters-1$pt$]=current&get=500?epgId=177";
+            string url = "https://www.mycanal.fr/";
+            //string url = "https://pass.canal-plus.com/form/authentication_sms_activation?bundle=site&ssoconf=auth_mini_actsms_myc&popin=true&pass_target=https%3A%2F%2Fwww.mycanal.fr%2F%23%3Forigref%3D&urlSource=https%3A%2F%2Fwww.mycanal.fr%2F%3Forigref%3D&pass_modal=false&socialLinksDisabled=true&omnitureChannel=Usage&distributorId=D22024";
+
+
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("disable-infobars");
+            chromeOptions.AddArgument(string.Format("user-data-dir={0}", "C:/Users/Chrys/AppData/Local/Google/Chrome/User Data"));
+
+            var driver = new ChromeDriver(chromeOptions);
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Window.FullScreen();
+
+            
+
+            WebDriverWait waitForElement = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            waitForElement.Until(ExpectedConditions.ElementIsVisible(By.Id("application")));
+
+            driver.Navigate().GoToUrl("https://www.mycanal.fr/live/");
+
+            //Find channel button
+            var canalplus = driver.FindElementByXPath("//img[contains(@src, '"+ chaines[chaine] +".PNG')]");
+            canalplus.Click();
+
+        }
+        
+        void test() {
+            //Login
+            //var loginButton = driver.FindElementByXPath("//a[@class='link___64Swd pass_popin pass_auth_mini_actsms_myc']");
+            //loginButton.Click();
+
+            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            /*driver.SwitchTo().Frame("pass_iframe");
+
+            var userNameField = driver.FindElementByXPath("//input[@name='ssoEmail']");
+            var userPasswordField = driver.FindElementByXPath("//input[@name='ssoPass']");
+
+            userNameField.SendKeys("christophe.aroule@gmail.com");
+            userPasswordField.SendKeys("raikkonen2007");
+
+            var loginButton = driver.FindElementByXPath("//input[@value='Valider']");
+            loginButton.Click();*/
+        }
+    }
+}
--- TvBox/Views/MainWindow.xaml.cs
+++ TvBox/Views/MainWindow.xaml.cs
@@ -2,6 +2,11 @@
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using TvBox.Models;
 
 namespace TvBox.Views
 {
@@ -11,7 +16,13 @@ namespace TvBox.Views
         public MainWindow()
         {
             InitializeComponent();
-            System.Diagnostics.Process.Start("C:\\Users\\Chris\\Documents\\GitHub\\TvBox\\TvBox\\chrome_debug.bat");
+
+            MyCanal myCanal = new MyCanal();
+            myCanal.testCanalPlus(4);
+
+            //testF1tv();
+            
+            
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -30,7 +41,8 @@ namespace TvBox.Views
             //chrome.NavigateTo("https://www.mycanal.fr/live/&params[tab]=/live-tv/pid5170-live-tv-v2-liste-des-chaines.html&params[filter]=0&params[filters-0$g$]=0$g$2$&params[filters-1$pt$]=current&get=500?epgId=177");
             var button = sender as Button;
             var code = button.Tag;
-            chrome.NavigateTo(code.ToString());
+            //chrome.NavigateTo(code.ToString());
+            chrome.Eval("window.location='"+ code.ToString() +"';");
 
             //chrome.Eval("document.querySelector('[data-sp-action=\"fullscreenBtPressed\"]').click();");
 
@@ -54,6 +66,55 @@ namespace TvBox.Views
             chrome.SetActiveSession(sessionWSEndpoint);
             chrome.Eval("window.close()");
             System.Windows.Application.Current.Shutdown();
+        }
+        
+
+        private void testF1tv()
+        {
+            string url = "https://account.formula1.com/#/FR/login";
+            
+
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("disable-infobars");
+
+            var driver = new ChromeDriver(chromeOptions);
+
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Window.FullScreen();
+
+            //Login
+            var userNameField = driver.FindElementByXPath("//input[@name='Login']");
+            var userPasswordField = driver.FindElementByXPath("//input[@name='Password']");
+            userNameField.SendKeys("ramzi.haddad00@gmail.com");
+            userPasswordField.SendKeys("raikkonen2007");
+            var loginButton = driver.FindElementByXPath("//button[@type='submit']");
+            loginButton.Click();
+
+
+            WebDriverWait waitForElement = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            waitForElement.Until(ExpectedConditions.ElementIsVisible(By.Id("accountManagementDiv")));
+
+            driver.Navigate().GoToUrl("https://f1tv.formula1.com/FR/");
+
+            //Play live
+            WebDriverWait waitForElement2 = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            waitForElement2.Until(ExpectedConditions.ElementIsVisible(By.ClassName("episode__circle-wrapper-__-IiO58")));
+
+            var playLive = driver.FindElementByClassName("episode__circle-wrapper-__-IiO58");
+            //playLive.Click();
+
+            var playButton = playLive.FindElement(By.TagName("a"));
+            playButton.Click();
+
+            //Play Raikko
+            var raikko = driver.FindElementByXPath("//div[text()='RAI']");
+            raikko.Click();
+        }
+
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            textbox1.Text = "You Entered: " + e.Key.ToString();
+        
         }
     }
 }
